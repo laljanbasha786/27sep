61755a5148b71093ff4a43a88f5db6f03e20ce73
laljanbasha786 <laljanbasha05@gmail.com>
2024-10-04 21:51:38 +0530

    Add files via upload

---
+++ 27sep.js
@@ -0,0 +1,65 @@
+// var a=[1,2,3,4,5];
+// var res=a.length;
+// console.log(res);
+
+
+//1.push()
+//2.pop()
+//3.unshift()
+//4.shift()
+//5.reverse)()
+//6.index of()
+//7.includes()
+//8.sort()
+//9.join()
+
+//1.Ans
+// var a=[1,2,3,4];
+// console.log(a);
+
+// a.push(10);
+// console.log(a);
+
+// a.shift()
+// console.log(a);
+
+
+//2.Ans
+// var a=[1,2,3,4,5];
+// console.log(a);
+
+// a.reverse();
+// console.log(a);
+
+// var res=a.join()
+// console.log(", ",res);
+
+//3.Ans
+// var a=[9,3,2,0,1];
+// a=a.sort((a,b) => a-b);
+
+// console.log(a);
+// a.pop();
+// console.log(a);
+
+//4.Ans
+
+// var a=[3,4,5];
+// a.unshift(1,2);
+// console.log(a);
+// a.shift(a);
+
+// console.log(a);
+// var res=a.length;
+// console.log(res);
+
+// //5.Ans
+// var a=[2,4,5];
+// var b=[9,7,2];
+// var res=a.concat(b);
+// console.log(res);
+
+// res.sort()
+// console.log(res);
+// res.pop();
+// console.log(res);
